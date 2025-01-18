52a0b712963a571a6d8baf9ecc57346f2827d3d8
Rahman2001 <rahmanrejepov777@gmail.com>
2025-01-18 22:38:57 +0300

    added google oauth api methods for oauth authorization

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
 import logging
 import sys
+from uuid import uuid4
 
-from flask import Flask, url_for, session, redirect
+from flask import Flask, redirect
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
 from flask_session import Session
 
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import Flow
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 from model import *
@@ -26,40 +25,25 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s -
 
 @app.route('/api/docurag/oauth/authorize', methods=['GET', 'POST'])
 def authorize():
+    docusign_state = request.args.get('state')
     # Create the OAuth flow object
     flow = InstalledAppFlow.from_client_secrets_file(
         OAUTH_CLIENT_CREDENTIALS_PATH, scopes=OAUTH_SCOPES)
-    flow.redirect_uri = url_for('callback', _external=True)
-    authorization_url, state = flow.authorization_url(
+    flow.redirect_uri = request.args.get("redirect_uri")
+    authorization_url, google_state = flow.authorization_url(
         access_type='offline',
-        prompt='select_account')
-
-    # Save the state so we can verify the request later
-    session['state'] = state
-    print(f'session state: {state}')
+        prompt='select_account',
+        state=docusign_state
+    )
     return redirect(authorization_url)
 
 
-@app.route('/api/docurag/oauth/callback', methods=['GET', 'POST'])
-def callback():
-    # Verify the request state
-    if request.args.get('state') != session['state']:
-        raise Exception('Invalid state')
-
-    # Create the OAuth flow object
-    flow = InstalledAppFlow.from_client_secrets_file(
-        OAUTH_CLIENT_CREDENTIALS_PATH, scopes=OAUTH_SCOPES, state=session['state'])
-    flow.redirect_uri = url_for('callback', _external=True)
-
-    # Exchange the authorization code for an access token
-    authorization_response = request.url
-    flow.fetch_token(authorization_response=authorization_response)
-
-    # Save the credentials to the session
-    credentials = flow.credentials
-    session['credentials'] = dict(credentials)
-
-    return redirect(url_for('index'))
+@app.route('/api/docurag/oauth/token', methods=['GET', 'POST'])
+def token():
+    print(f'request url : {request.url}')
+    access_token = uuid4()
+    refresh_token = uuid4()
+    return {'access_token': access_token, 'refresh_token': refresh_token}
 
 
 @app.route('/api/question', methods=['GET'])
